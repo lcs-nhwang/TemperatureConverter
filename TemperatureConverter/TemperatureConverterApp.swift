153c9422cae2fa3ac7c499dcce92580152a4b26b
lcs-nhwang <sjnhwang@lcs.on.ca>
2025-03-03 08:35:53 -0500

    Initial Commit

---
+++ TemperatureConverter/TemperatureConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TemperatureConverterApp.swift
+//  TemperatureConverter
+//
+//  Created by Nicholas Hwang on 3/3/2025.
+//
+
+import SwiftUI
+
+@main
+struct TemperatureConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
